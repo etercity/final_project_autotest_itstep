58ff870c941ce47fa794f663d82f1b6da2950b27
etercity <etercity@gmail.com>
2023-11-01 14:09:43 +0200

    add test_main_page

---
+++ pages/main_page.py
@@ -0,0 +1,6 @@
+
+
+
+
+class MainPage:
+    pass
---
+++ tests/test_main_page.py
@@ -0,0 +1,31 @@
+import pytest
+from pages.base_page import BasePage
+from pages.main_page import MainPage
+from settings import sets
+
+
+@pytest.mark.smoke
+@pytest.mark.regression
+@pytest.mark.main_page
+class TestMainPage:
+
+    def setup_method(self):
+        pass
+
+    def test_get_main_page(self, browser):
+        page = BasePage(browser, sets.PROD_SERVER)
+        page.open()
+
+    def test_login_logout(self, browser):
+        link_to_site = browser.current_url
+        page = BasePage(browser, link_to_site)
+
+
+
+
+
+
+
+
+
+
